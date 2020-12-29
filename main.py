c81658816a20d7b900d154ecd08b0a4bfe2f2d13
Blake <yourlocalblake@gmail.com>
2020-12-29 19:01:15 +1100

    Quick and dirty entry point

---
+++ main.py
@@ -0,0 +1,23 @@
+# Main file currently used for calling models.
+
+from DEModelling.solution import calc_numerical_solution
+from numpy import linspace
+from DEModelling.utils import ODEIndex
+
+import matplotlib.pyplot as plt
+
+time_start = 1
+time_end = 20
+num_points = 1000
+initial_x = 1
+inital_xdot = 1
+initial_conditions = [initial_x, inital_xdot]
+
+
+sol = calc_numerical_solution(initial_conditions=initial_conditions, time=time_start, tstop=time_end)
+
+y_points = [i[0] for i in sol.values.y]
+ydot_point = [i[1] for i in sol.values.y]
+
+plt.plot(linspace(time_start, time_end, num_points), y_points)
+plt.show()
