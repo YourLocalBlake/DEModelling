5efe63bf60460f6bd9eb4e8dd2a00549c7c6c5d3
Blake <yourlocalblake@gmail.com>
2021-01-15 13:24:48 +1100

    Update to new var convention

--- src/DEModelling/models/one_var.py
+++ src/DEModelling/models/one_var.py
@@ -5,6 +5,7 @@ def model_const_0_func():
     """
     return 0, 0
 
+
 def model_const_1_func():
     """ A model where the derivative changes as constant 1.
     """
@@ -17,146 +18,145 @@ def model_const_2_func():
     return 2, 2
 
 
-def model_x_func(*, x):
-    """ A model where the x derivative changes as x.
+def model_x1_func(*, x1):
+    """ A model where the x1 derivative changes as x1.
     """
-    return x, x
+    return x1, x1
 
 
-def model_x_deriv_func(*, xdot):
-    """ A model where the x derivative changes as the derivative of the x function.
+def model_x1_deriv_func(*, x1dot):
+    """ A model where the x1 derivative changes as the derivative of the x1 function.
     """
-    return xdot, xdot
+    return x1dot, x1dot
 
 
-def model_x2_func(*, x):
-    """ A model where the x derivative changes as x squared.
+def model_x1_squared_func(*, x1):
+    """ A model where the x1 derivative changes as x1 squared.
     """
-    return x**2, x**2
+    return x1**2, x1**2
 
 
-def model_x2_deriv_func(*, xdot):
-    """ A model where the x derivative changes as the derivative of the x function squared.
+def model_x1_squared_deriv_func(*, x1dot):
+    """ A model where the x1 derivative changes as the derivative of the x1 function squared.
     """
-    return xdot**2, xdot**2
+    return x1dot**2, x1dot**2
 
 
-def model_x3_func(*, x):
-    """ A model where the x derivative changes as x cubed.
+def model_x1_cubed_func(*, x1):
+    """ A model where the x1 derivative changes as x1 cubed.
     """
-    return x**3, x**3
+    return x1**3, x1**3
 
 
-def model_x3_deriv_func(*, xdot):
-    """ A model where the x derivative changes as the derivative of the x function cubed.
+def model_x1_cubed_deriv_func(*, x1dot):
+    """ A model where the x1 derivative changes as the derivative of the x1 function cubed.
     """
-    return xdot**3, xdot**3
+    return x1dot**3, x1dot**3
 
 
-def model_2x_func(*, x):
-    """ A model where the x derivative changes as 2 times x.
+def model_2x1_func(*, x1):
+    """ A model where the x1 derivative changes as 2 times x1.
     """
-    return 2*x, 2*x
+    return 2*x1, 2*x1
 
 
-def model_2x_deriv_func(*, xdot):
-    """ A model where the x derivative changes as 2 times the derivative of the x function.
+def model_2x1_deriv_func(*, x1dot):
+    """ A model where the x1 derivative changes as 2 times the derivative of the x1 function.
     """
-    return 2*xdot, 2*xdot
+    return 2*x1dot, 2*x1dot
 
 
-def model_y_func(*, y):
-    """ A model where the y derivative changes as y.
+def model_x2_func(*, x2):
+    """ A model where the x2 derivative changes as x2.
     """
-    return y, y
+    return x2, x2
 
 
-def model_y_deriv_func(*, ydot):
-    """ A model where the y derivative changes as the derivative of the y function.
+def model_x2_deriv_func(*, x2dot):
+    """ A model where the x2 derivative changes as the derivative of the x2 function.
     """
-    return ydot, ydot
+    return x2dot, x2dot
 
 
-def model_y2_func(*, y):
-    """ A model where the y derivative changes as y squared.
+def model_x2_squared_func(*, x2):
+    """ A model where the x2 derivative changes as x2 squared.
     """
-    return y**2, y**2
+    return x2**2, x2**2
 
 
-def model_y2_deriv_func(*, ydot):
-    """ A model where the y derivative changes as the derivative of the y function squared.
+def model_x2_squared_deriv_func(*, x2dot):
+    """ A model where the x2 derivative changes as the derivative of the x2 function squared.
     """
-    return ydot**2, ydot**2
+    return x2dot**2, x2dot**2
 
 
-def model_y3_func(*, y):
-    """ A model where the y derivative changes as y cubed.
+def model_x2_cubed_func(*, x2):
+    """ A model where the x2 derivative changes as x2 cubed.
     """
-    return y**3, y**3
+    return x2**3, x2**3
 
 
-def model_y3_deriv_func(*, ydot):
-    """ A model where the y derivative changes as the derivative of the y function cubed.
+def model_x2_cubed_deriv_func(*, x2dot):
+    """ A model where the x2 derivative changes as the derivative of the x2 function cubed.
     """
-    return ydot**3, ydot**3
+    return x2dot**3, x2dot**3
 
 
-def model_2y_func(*, y):
-    """ A model where the y derivative changes as 2 times y.
+def model_2x_squared_func(*, x2):
+    """ A model where the x2 derivative changes as 2 times x2.
     """
-    return 2*y, 2*y
+    return 2*x2, 2*x2
 
 
-def model_2y_deriv_func(*, ydot):
-    """ A model where the y derivative changes as 2 times the derivative of the y function.
+def model_2x2_deriv_func(*, x2dot):
+    """ A model where the x2 derivative changes as 2 times the derivative of the x2 function.
     """
-    return 2*ydot, 2*ydot
+    return 2*x2dot, 2*x2dot
 
 
-def model_z_func(*, z):
-    """ A model where the derivative changes as z.
+def model_x3_func(*, x3):
+    """ A model where the derivative changes as x3.
     """
-    return z, z
+    return x3, x3
 
 
-def model_z_deriv_func(*, zdot):
-    """ A model where the derivative changes as the derivative of the z function.
+def model_x3_deriv_func(*, x3dot):
+    """ A model where the derivative changes as the derivative of the x3 function.
     """
-    return zdot, zdot
+    return x3dot, x3dot
 
 
-def model_z2_func(*, z):
-    """ A model where the derivative changes as z squared.
+def model_x3_squared_func(*, x3):
+    """ A model where the derivative changes as x3 squared.
     """
-    return z**2, z**2
+    return x3**2, x3**2
 
 
-def model_z2_deriv_func(*, zdot):
-    """ A model where the derivative changes as the derivative of the z function squared.
+def model_x3_squared_deriv_func(*, x3dot):
+    """ A model where the derivative changes as the derivative of the x3 function squared.
     """
-    return zdot**2, zdot**2
+    return x3dot**2, x3dot**2
 
 
-def model_z3_func(*, z):
-    """ A model where the derivative changes as z cubed.
+def model_x3_cubed_func(*, x3):
+    """ A model where the derivative changes as x3 cubed.
     """
-    return z**3, z**3
+    return x3**3, x3**3
 
 
-def model_z3_deriv_func(*, zdot):
-    """ A model where the derivative changes as the derivative of the z function cubed.
+def model_x3_cubed_deriv_func(*, x3dot):
+    """ A model where the derivative changes as the derivative of the x3 function cubed.
     """
-    return zdot**3, zdot**3
+    return x3dot**3, x3dot**3
 
 
-def model_2z_func(*, z):
-    """ A model where the derivative changes as 2 times z.
+def model_2x3_func(*, x3):
+    """ A model where the derivative changes as 2 times x3.
     """
-    return 2*z, 2*z
+    return 2*x3, 2*x3
 
 
-def model_2z_deriv_func(*, zdot):
-    """ A model where the derivative changes as 2 times the derivative of the z function.
+def model_2x3_deriv_func(*, x3dot):
+    """ A model where the derivative changes as 2 times the derivative of the x3 function.
     """
-    return 2*zdot, 2*zdot
-
+    return 2*x3dot, 2*x3dot
