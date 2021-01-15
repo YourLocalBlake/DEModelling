4b9c50b5728ac77503152612cc5349280483a8ec
Blake <yourlocalblake@gmail.com>
2021-01-15 13:27:15 +1100

    Update to new var convention

--- src/DEModelling/utils.py
+++ src/DEModelling/utils.py
@@ -6,35 +6,39 @@ class ODEIndex(IntEnum):
     Enumerated number for array index for variables in the ODEs
     """
     # Dimensionful units
-    a1 = 0
-    a1dot = 1
-    a2 = 2
-    a2dot = 3
-    a3 = 4
-    a3dot = 5
-    x = 0
-    xdot = 1
-    y = 2
-    ydot = 3
-    z = 4
-    zdot = 5
+    x1 = 0
+    x1dot = 1
+    x1ddot = 2
+    x2 = 3
+    x2dot = 4
+    x2ddot = 5
+    x3 = 6
+    x3dot = 7
+    x3ddot = 8
+    x4 = 9
+    x4dot = 10
+    x5 = 11
+    x5dot = 12
+    x5ddot = 13
 
 
 class InitConIndex(IntEnum):
     """
     Enumerated number for array index for variables in the initial conditions object.
     """
-    a1 = 0
-    a1dot = 1
-    a2 = 2
-    a2dot = 3
-    a3 = 4
-    a3dot = 5
-    x = 0
-    xdot = 1
-    y = 2
-    ydot = 3
-    z = 4
-    zdot = 5
+    x1 = 0
+    x1dot = 1
+    x1ddot = 2
+    x2 = 3
+    x2dot = 4
+    x2ddot = 5
+    x3 = 6
+    x3dot = 7
+    x3ddot = 8
+    x4 = 9
+    x4dot = 10
+    x5 = 11
+    x5dot = 12
+    x5ddot = 13
 
 
