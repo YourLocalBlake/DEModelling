6d46ab21784ef7c3db0b1464d3c53a962fd96475
Blake <yourlocalblake@gmail.com>
2021-01-15 13:31:50 +1100

    Update to new var convention

--- src/DEModelling/initialiser.py
+++ src/DEModelling/initialiser.py
@@ -32,24 +32,24 @@ def ode_system(*, store_internal_data=True):
         Inputs are passed by Sundials.
         """
         # Specify the variables of the system.
-        x = params[ODEIndex.x]
-        y = params[ODEIndex.y]
-        z = params[ODEIndex.z]
-        xdot = params[ODEIndex.xdot]
-        ydot = params[ODEIndex.ydot]
-        zdot = params[ODEIndex.zdot]
+        x1 = params[ODEIndex.x1]
+        x2 = params[ODEIndex.x2]
+        x3 = params[ODEIndex.x3]
+        x1dot = params[ODEIndex.x1dot]
+        x2dot = params[ODEIndex.x2dot]
+        x3dot = params[ODEIndex.x3dot]
 
         # First order derivative functions
         # Place here the RHS of the equation.
-        # i.e. x' = 2x + 3 would be deriv_x = model_2x_plus_3_func()
-        deriv_x, deriv_x_i = 1, 1
-        deriv_y, y_i = 2, 2
-        deriv_z, z_i = 3, 3
+        # i.e. x' = 2x + 3 would be deriv_x = model_2x1_plus_3_func()
+        deriv_x1, x1_i = model_x1_func(x1=x1)
+        deriv_x2, x2_i = model_x2_func(x2=x2)
+        deriv_x3, x3_i = model_x3_func(x3=x3)
 
         # Solve
-        derivs[ODEIndex.x] = deriv_x
-        derivs[ODEIndex.y] = deriv_y
-        derivs[ODEIndex.z] = deriv_z
+        derivs[ODEIndex.x1] = deriv_x1
+        derivs[ODEIndex.x2] = deriv_x2
+        derivs[ODEIndex.x3] = deriv_x3
 
     return rhs_equation
 
